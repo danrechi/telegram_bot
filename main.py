85f0a937d619c3a4826ae5099841e2a4f8bd6e29
Rivaric <helium@internet.ru>
2023-09-15 13:57:30 +0300

    removed text

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ def start(message):
 
 @bot.message_handler(commands=['help'])
 def start_message(message):
-    bot.send_message(message.chat.id, "Бот предназначен для получения расписания для "
+    bot.send_message(message.chat.id, ""
                                       "группы БВТ2203."
                                       "\nТакже в боте есть несколько команд:"
                                       "\n/start - чтобы вернуться к приветствию"
